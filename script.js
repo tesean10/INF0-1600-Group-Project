2a89df967662dc302a7101db5f3f4dcaa0d25769
lele-droid <leler4521@gmail.com>
2026-04-11 10:57:14 -0400

    modal working

--- script.js
+++ script.js
@@ -43,10 +43,11 @@ function displayMovies(movies) {
         </button>
         </div>
         `;
-        container.appendChild(movieElement);
-    });
+        
+         movieElement.onclick = () => showMovieDetails(movie);
 
-    movieElement.onclick = () => showMovieDetails(movie);
+         container.appendChild(movieElement);
+    });
 }
 
 // Search Movies (Local Filter)
@@ -107,21 +108,41 @@ function addToFavourites()
 }
 
 //movie modal
-function showMovieDetails(movie)
+function showMovieDetails(movie) 
 {
     const modal = document.getElementById("movie-modal");
     const content = document.getElementById("modal-content");
 
     content.innerHTML = `
-    <h2>${movie.title}</h2>
-    <img src="${IMG_PATH + movie.poster_path}">
-    <p>${movie.overview}</p>
-    <button onclick="closeModal()">close</button>
+        <div class="modal-layout">
+            <div class="modal-left">
+                <h2>${movie.title}</h2>
+                <img src="${IMG_PATH + movie.poster_path}" alt="${movie.title}">
+                <p><strong>Rating:</strong> ${movie.vote_average}</p>
+                <p><strong>Year:</strong> ${
+                    movie.release_date
+                        ? movie.release_date.split('-')[0]
+                        : 'N/A'
+                }</p>
+                <button onclick="addToFavourites(${movie.id})">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-heart" viewBox="0 0 16 16">
+                         <path fill-rule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
+                         <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+                    </svg>
+                </button>
+            </div>
+
+            <div class="modal-right">
+                <h3>Synopsis</h3>
+                <p>${movie.overview}</p>
+            </div>
+        </div>
+
+        <button class="close-btn" onclick="closeModal()">Close</button>
     `;
 
     modal.classList.add("show");
 }
-
 function closeModal()
 {
     document.getElementById("movie-modal").classList.remove("show");
