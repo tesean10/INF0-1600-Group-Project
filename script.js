39fd404f7a017c78ac5b4e3a83556966d3087f75
tesean10 <teseanmelville10@gmail.com>
2026-04-11 21:10:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -35,12 +35,13 @@ function displayMovies(movies) {
         <img src="${poster_path ? IMG_PATH + poster_path : 'https://via.placeholder.com/500x750'}" alt="${title}">
         <div class="movie-info">
         <h3>${title}</h3>
-        <span class="rating">${vote_average}</span>
+        <span class="rating">${vote_average.toFixed(1)}</span>
         <p>${release_date ? release_date.split('-')[0] : 'N/A'}</p>
         <button onclick="addToFavourites('${movie.id}')"> 
              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark-heart" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z"/>
-                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+                <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1
+
             </svg>
         </button>
         </div>
@@ -67,7 +68,25 @@ function searchMovies() {
     );
     displayMovies(filteredMovies);
 }
+// API Search Logic Entire catalouge
+async function searchMovies() {
+    const query = document.getElementById('search-input').value;
+    
+    if (query.trim() === "") {
+        getMovies(); // If empty, show top rated
+        return;
+    }
 
+    try {
+        const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`;
+        const res = await fetch(SEARCH_URL);
+        const data = await res.json();
+        
+        displayMovies(data.results);
+    } catch (error) {
+        console.error("Search failed:", error);
+    }
+}
 // Sort Movies
 function sortMovies(type) {
     let sorted = [...movieData];
